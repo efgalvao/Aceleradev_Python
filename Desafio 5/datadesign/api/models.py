6c00b4e0db3bed9401757996dde88a5630416fb1
efgalvao <solfieri@hotmail.com>
2020-06-12 20:41:23 -0300

    Updates.

--- Desafio 5/datadesign/api/models.py
+++ Desafio 5/datadesign/api/models.py
@@ -2,21 +2,21 @@ from django.db import models
 from django.core import validators
 
 class User(models.Model, ):
-    name = models.CharField(max_length=50)
-    last_login = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(max_length=254)
-    password = models.CharField(max_length=50, validators=[validators.MinValueValidator(8)])
+    name = models.CharField('Nome', max_length=50)
+    last_login = models.DateTimeField('Último login', auto_now_add=True)
+    email = models.EmailField('E-mail', max_length=254)
+    password = models.CharField('Senha', max_length=50, validators=[validators.MinLengthValidator(8)])
 
 
 class Agent(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('Nome', max_length=50)
     status = models.BooleanField()
     env = models.CharField(max_length=20)
-    version = models.CharField(max_length=5)
-    address = models.GenericIPAddressField(protocol="IPV4", default="0.0.0.0")
+    version = models.CharField('Version', max_length=5)
+    address = models.GenericIPAddressField('Address', protocol="IPV4", default="0.0.0.0")
 
 class Group(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField('Nome', max_length=50)
 
 
 class Event(models.Model):
@@ -28,9 +28,9 @@ class Event(models.Model):
         ('INFO', 'INFO'),
     ]
     level = models.CharField(max_length=20, choices=ERROR_LEVEL)
-    data = models.TextField()
-    arquivado = models.BooleanField(default=False)
-    date = models.DateField(auto_now=True)
+    data = models.TextField("Dados")
+    arquivado = models.BooleanField('Arquivado', default=False)
+    date = models.DateField('Data', auto_now=True)
     agent = models.OneToOneField(Agent, on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
@@ -38,5 +38,3 @@ class Event(models.Model):
 class GroupUser(models.Model):
     group = models.OneToOneField(Group, on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
