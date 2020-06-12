ebba2eb07dfe83ab135b94a0381643744f9b174a
efgalvao <solfieri@hotmail.com>
2020-06-12 01:47:41 -0300

    Models,settings and admin updated.

--- Desafio 5/datadesign/api/admin.py
+++ Desafio 5/datadesign/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Agent, Event, Group, GroupUser
+
+admin.site.register(User)
+admin.site.register(Agent)
+admin.site.register(Event)
+admin.site.register(Group)
+admin.site.register(GroupUser)
+
+
--- Desafio 5/datadesign/api/models.py
+++ Desafio 5/datadesign/api/models.py
@@ -1,55 +1,42 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
-class api_user(models.Model):
+class User(models.Model, ):
     name = models.CharField(max_length=50)
-    last_login = models.DateTimeField
-    e-mail = models.CharField(max_length=254)
-    password = models.CharField(max_length=50)
+    last_login = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=254, validators=[validators.EmailValidator()])
+    password = models.CharField(max_length=50, validators=[validators.MinValueValidator(8)])
 
-class api_agent(models.Model):
+
+class Agent(models.Model):
     name = models.CharField(max_length=50)
-    status = models.BooleanField
+    status = models.BooleanField()
     env = models.CharField(max_length=20)
     version = models.CharField(max_length=5)
-    addres = models.CharField(max_length=39)
+    address = models.GenericIPAddressField(protocol="IPV4", default="0.0.0.0")
 
-class api_group(models.Model):
+class Group(models.Model):
     name = models.CharField(max_length=50)
-    
-class api_event(models.Model):
-    level = models.CharField(max_length=20)
-    data = models.TextField
-    arquivado = models.BooleanField
-    date = models.DateField
-    agent_id = models.IntegerField
-    user_id = models.IntegerField
-
-class api_grouuser(models.Model):
-    group = models.IntegerField
-    user_id =models.IntegerField
-    
 
 
-# Modelos do Mozilla
-'''
-class MyModelName(models.Model):
-    """Uma típica classe definindo um modelo, derivada da classe Model."""
+class Event(models.Model):
+    ERROR_LEVEL = [
+        ('CRITICAL', 'CRITICAL'),
+        ('DEBUG', 'DEBUG'),
+        ('ERROR', 'ERROR'),
+        ('WARNING', 'WARNING'),
+        ('INFO', 'INFO'),
+    ]
+    level = models.CharField(max_length=20, choices=ERROR_LEVEL)
+    data = models.TextField()
+    arquivado = models.BooleanField(default=False)
+    date = models.DateField(auto_now=True)
+    agent = models.OneToOneField(Agent, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    # Campos
-    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
-    ...
 
-    # Metadados
-    class Meta: 
-        ordering = ['-my_field_name']
+class GroupUser(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    # Métodos
-    def get_absolute_url(self):
-        """Retorna a url para acessar uma instancia específica de MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
-    
-    def __str__(self):
-        """ String para representar o objeto MyModelName (no site Admin)."""
-        return self.my_field_name
-'''
+
