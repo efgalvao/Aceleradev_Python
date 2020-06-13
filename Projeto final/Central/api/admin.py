0024282bd4c88088f75030231d599648eb80d31e
efgalvao <solfieri@hotmail.com>
2020-06-12 21:26:18 -0300

    Projeto final WIP,

--- Projeto final/Central/api/admin.py
+++ Projeto final/Central/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+admin.site.register(User, UserAdmin)
+
+# Old
+
+from .models import User2, Agent, Event, Group, GroupUser
+
+admin.site.register(User2)
+admin.site.register(Agent)
+admin.site.register(Event)
+admin.site.register(Group)
+admin.site.register(GroupUser)
+
--- Projeto final/Central/api/models.py
+++ Projeto final/Central/api/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+# OLD
+from django.core import validators
+
+class User2(models.Model, ):
+    name = models.CharField('Nome', max_length=50)
+    last_login = models.DateTimeField('Último login', auto_now_add=True)
+    email = models.EmailField('E-mail', max_length=254, unique=True)
+    password = models.CharField('Senha', max_length=50, validators=[validators.MinLengthValidator(8)])
+
+
+class Agent(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    status = models.BooleanField()
+    env = models.CharField(max_length=20)
+    version = models.CharField('Version', max_length=5)
+    address = models.GenericIPAddressField('Address', protocol="IPV4", default="0.0.0.0")
+
+class Group(models.Model):
+    name = models.CharField('Nome', max_length=50)
+
+
+class Event(models.Model):
+    ERROR_LEVEL = [
+        ('CRITICAL', 'CRITICAL'),
+        ('DEBUG', 'DEBUG'),
+        ('ERROR', 'ERROR'),
+        ('WARNING', 'WARNING'),
+        ('INFO', 'INFO'),
+    ]
+    level = models.CharField(max_length=20, choices=ERROR_LEVEL)
+    data = models.TextField("Dados")
+    arquivado = models.BooleanField('Arquivado', default=False)
+    date = models.DateField('Data', auto_now=True)
+    agent = models.OneToOneField(Agent, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class GroupUser(models.Model):
+    group = models.OneToOneField(Group, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
