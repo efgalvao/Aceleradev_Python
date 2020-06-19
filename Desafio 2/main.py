4ff07f8d2b585678005f2c11e9ba5785342e26e1
efgalvao <solfieri@hotmail.com>
2020-06-19 20:03:08 -0300

    Challenge 2 updated.

--- Desafio 2/main.py
+++ Desafio 2/main.py
@@ -1,101 +1,99 @@
 from abc import ABC, abstractmethod
 
 class Department:
-
     """
-    Classe usada para definir a qual departamento o funcionário pertence.
+    Class used to define which department the employee belongs to.
 
-    Atributos
+    Attributes
 
     name: str
-        Nome do departamento.
+        Department name.
     code: int
-        Código do departamento.
+        Department code.
     """
 
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
-
 class Employee(ABC):
     """
-    Classe usada para criação de objetos da Classe Employee.
+    Class used to create objects of the Employee Class.
 
-    Atributos
+    Attributes
 
     code: int
-        Código do funcionário.
+        Employee code.
     name: str
-        Nome do funcionário.
+        Employee name.
     salary : int
-        Salário do funcionário.
-    hours :     
-        Carga horária do funcionário
+        Employee salary.
+    workload :     
+        Employee workload.
 
     """
     def __init__(self, code, name, salary):
         self._code = code
         self._name = name
         self._salary = salary
-        self._hours = 8
+        self._workload = 8
 
     @abstractmethod
-    def calc_bonus(self): # Função utilizada para calcular bonus de funcionários
+    def calc_bonus(self): # Function used to calculate employee bonuses
         pass
 
     @abstractmethod
-    def get_hours(self): # Função utilizada para retornar a carga horária do funcionário
+    def get_hours(self): # Function used to return the employee's workload
         pass
 
 class Manager(Employee):
     """
     Classe usada para criação de objetos da Classe Manager.
 
-    Atributos
+    Attributes
 
     code: int
-        Código do funcionário.
+        Employee code.
     name: str
-        Nome do funcionário.
+        Employee name.
     salary : int
-        Salário do funcionário.
+        Employee salary.
     department :     
-        Departamento ao qual o funcionário pertence.
+        Employee dapartment.
 
     """
     def __init__(self, code, name, salary):
         super().__init__(code, name, salary)
         self._department = Department('managers', 1)
 
-    def calc_bonus(self): # Função utilizada para calcular bonus de funcionários da classe Manager
+    def calc_bonus(self): # Function used to calculate bonus for Manager class employees
         return self._salary * 0.15
 
-    def get_hours(self): # Função utilizada para retornar a carga horária do funcionário
-        return self._hours
+    def get_hours(self): # Function used to return the employee's workload
+        return self._workload
 
-    def get_department(self): # Função utilizada para retornar o departamento do funcionário
+    def get_department(self): # Function used to return the employee's department
         return self._department.name
 
-    def set_department(self, department): # Função utilizada para definir o departamento do funcionário
+    def set_department(self, department): # Function used to define the employee's department
         self._department.name = department
 
 class Seller(Employee):
     """
-    Classe usada para criação de objetos da Classe Seller.
+    Class used to create objects of the Seller Class.
 
-    Atributos
+    Attributes
 
     code: int
-        Código do funcionário.
+        Employee code.
     name: str
-        Nome do funcionário.
+        Employee name.
     salary : int
-        Salário do funcionário.
+        Employee salary.
     department :     
-        Departamento ao qual o funcionário pertence.
+        Employee department.
     sales : int
-        Valor total de vendas do funcionário.
+        Total employee sales value.
 
     """    
     def __init__(self, code, name, salary):
@@ -103,20 +101,20 @@ class Seller(Employee):
         self._department = Department('sellers', 2)
         self.__sales = 0
 
-    def calc_bonus(self): # Função utilizada para calcular bonus de funcionários da classe Seller
+    def calc_bonus(self): # Function used to calculate bonus for Seller class employees
         return (self.__sales * 0.15)
 
-    def get_hours(self): # Função utilizada para retornar a carga horária do funcionário
-        return self._hours
+    def get_hours(self): # Function used to return the employee's workload
+        return self._workload
 
-    def get_department(self): # Função utilizada para retornar o departamento do funcionário
+    def get_department(self): # Function used to return the employee's department
         return self._department.name
 
-    def set_department(self, department): # Função utilizada para definir o departamento do funcionário
+    def set_department(self, department): # Function used to define the employee's department
         self._department.name = department
 
-    def get_sales(self): # Função utilizada para retornar as vendas do funcionário
+    def get_sales(self): # Function used to return employee sales
         return self.__sales
 
-    def put_sales(self, value): # Função utilizada para cadastrar as vendas do Vendedor
+    def put_sales(self, value): # Function used to register Salesperson sales
         self.__sales += value
