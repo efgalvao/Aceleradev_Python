390fc7d8db60ddd9542a16c903fde5bff6f6d5bc
efgalvao <solfieri@hotmail.com>
2020-06-19 20:26:42 -0300

    Challenge 3 updated.

--- Desafio 3/main.py
+++ Desafio 3/main.py
@@ -2,11 +2,24 @@ import requests
 
 
 def get_temperature(lat, lng):
+    """
+    Function that obtain the temperature of a region based on its latitude and longitude coordinates.
+
+    Parameters
+    lat: float
+        Latitude
+
+    lng: float
+        Longitude
+
+    Return:
+    Temperature in degrees celsius.
+    """
     key = 'e1ee55658d4a2b28c4841e373c3b3d87'
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
     reponse = requests.get(url)
     data = reponse.json()
     temperature = data.get('currently').get('temperature')
     if not temperature:
-        return
+        return  "Temperatura não encontrada" # Temperature not found
     return int((temperature - 32) * 5.0 / 9.0)
