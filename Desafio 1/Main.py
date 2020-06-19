f03ce068c077ad26f99bb294d4994ec09a192c6c
efgalvao <solfieri@hotmail.com>
2020-06-19 19:18:59 -0300

    Challenge 1 update.

--- Desafio 1/Main.py
+++ Desafio 1/Main.py
@@ -1,45 +1,114 @@
 from datetime import datetime
 
-def duracao_chamada(registro):
-    """ Função para calcular o tempo da chamada utilizando as timestamps do arquivo records."""
-    inicio = datetime.fromtimestamp(registro["start"])
-    fim = datetime.fromtimestamp(registro["end"])
-    limite1 = datetime.strptime("22:00:00", "%H:%M:%S")
-    limite2 = datetime.strptime("06:00:00", "%H:%M:%S")
-    if inicio.hour >= limite1.hour and inicio.minute >= 0 :
-        inicio = inicio.replace(hour=6, minute=00, second = 00)
-    elif inicio.hour <= limite2.hour:
-        inicio = inicio.replace(hour=6, minute=00, second = 00)
-    if fim.hour >= limite1.hour and fim.minute >= 0:
-        fim = fim.replace(hour=22, minute=00, second = 00)
-    elif fim.hour <= limite2.hour:
-        fim = fim.replace(hour=6, minute=00, second = 00)
-    duracao = fim - inicio
-    duracaototal = (int(duracao.seconds) // 60)
-    return duracaototal
+def call_time(record):
+    """ 
+    Function to calculate the time of the call using the timestamps of the records file.
 
+    Parameters
 
+    record : dict
+        Dictionary with the origin and timestamps of the beginning and end of calls.
 
+    Return
 
-def main(records):
-    """Função que cria a lista de telefones e valor total com o auxílio de outras funções."""
-    conta = []
+    total_minutes : int
+        Total call duration in minutes. 
+
+    """
+    start = datetime.fromtimestamp(record["start"])
+    end = datetime.fromtimestamp(record["end"])
+    if int(start.hour) >= 22 and int(start.minute) >= 00:
+        start = start.replace(hour=22, minute=00, second=00)
+    elif int(start.hour) <= 6:
+        start = start.replace(hour=6, minute=00, second=00)
+    if int(end.hour) >= 22 and int(end.minute) >= 0:
+        end = end.replace(hour=22, minute=00, second=00)
+    elif int(end.hour) <= 6:
+        end = end.replace(hour=6, minute=00, second=00)
+    duration = end - start
+    total_minutes = (int(duration.seconds) // 60)
+    return total_minutes
+
+def classify_by_phone_number(records):
+    """
+    Function that creates the phone list and total value with the help of other functions.
+
+    Parameters
+
+    records: list
+
+        List containing dictionaries with call informations.
+
+    Return
+    
+    bill : list
+
+        List with total values by source number.
+    """
+    bill = []
     for i in records:
         x = {}
-        print(type(i), "aqui")
-        duracao = duracao_chamada(i)
-        custo = round((duracao * 0.09) + 0.36, 2)
+        duration = call_time(i)
+        cost = round((duration * 0.09) + 0.36, 2)
         x["source"] = i["source"]
-        x["total"] = custo
-        check(x, custo, conta)
-    return conta
+        x["total"] = cost
+        check(x, cost, bill)
+        bill = sorted(bill, key = lambda i: i['total'], reverse=True)
+    return bill
 
-def check(source, custo, conta):
-    """" Função para adicionar valores a números de origem já cadastrados."""
-    for dic in conta:
+def check(source, cost, bill):
+    """ 
+    Function to add values to source numbers in the bill list.
+
+    Parameters
+
+    source : str
+        Call source number.
+
+    cost : int
+        Call value to be added. 
+
+    bill : list
+        List where the origin and cost of the call will be added.
+
+    Return
+
+    bill : list
+        List containin call sources and costs.    
+
+    """
+    for dic in bill:
         if source['source'] in dic.values():
-            dic["total"] += custo
-            return conta
-    conta.append(source)
-    return conta
+            dic["total"] = round(dic["total"] + cost, 2)
+            return bill
+    bill.append(source)
+    return bill
+records = [
 
+    {'source': '48-996355555', 'destination': '48-666666666', 'end': 1564610974, 'start': 1564610674},
+
+    {'source': '41-885633788', 'destination': '41-886383097', 'end': 1564506121, 'start': 1564504821},
+
+    {'source': '48-996383697', 'destination': '41-886383097', 'end': 1564630198, 'start': 1564629838},
+
+    {'source': '48-999999999', 'destination': '41-885633788', 'end': 1564697158, 'start': 1564696258},
+
+    {'source': '41-833333333', 'destination': '41-885633788', 'end': 1564707276, 'start': 1564704317},
+
+    {'source': '41-886383097', 'destination': '48-996384099', 'end': 1564505621, 'start': 1564504821},
+
+    {'source': '48-999999999', 'destination': '48-996383697', 'end': 1564505721, 'start': 1564504821},
+
+    {'source': '41-885633788', 'destination': '48-996384099', 'end': 1564505721, 'start': 1564504821},
+
+    {'source': '48-996355555', 'destination': '48-996383697', 'end': 1564505821, 'start': 1564504821},
+
+    {'source': '48-999999999', 'destination': '41-886383097', 'end': 1564610750, 'start': 1564610150},
+
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564505021, 'start': 1564504821},
+
+    {'source': '48-996383697', 'destination': '41-885633788', 'end': 1564627800, 'start': 1564626000}
+
+]
+
+print(classify_by_phone_number(records))
