6013f782a881fccea58877d9dae069ed1f116501
efgalvao <solfieri@hotmail.com>
2020-06-24 12:14:39 -0300

    main updated.

--- Desafio 7/main.py
+++ Desafio 7/main.py
@@ -3,7 +3,8 @@ from datetime import date, timedelta
 
 
 def get_active_users() -> User:
-    """Traga todos os uarios ativos, seu último login deve ser menor que 10 dias """
+    """Traga todos os uarios ativos,
+    seu último login deve ser menor que 10 dias """
     days_ago = date.today() - timedelta(days=10)
     queryset = User.objects.filter(last_login__gt=days_ago)
     return queryset
@@ -29,17 +30,18 @@ def get_all_debug_events() -> Event:
 
 def get_all_critical_events_by_user(agent) -> Event:
     """Traga todos os eventos do tipo critico de um usuário específico"""
-    queryset = Event.objects.filter(level="critical") & Event.objects.filter(agent=agent)
+    queryset = Event.objects.filter(level="critical", agent=agent)
     return queryset
 
 
 def get_all_agents_by_user(username) -> Agent:
-    """Traga todos os agentes de associados a um usuário pelo nome do usuário"""
+    """Traga todos os agentes de associados a um usuário pelo nome"""
     queryset = Agent.objects.filter(user__name=username)
     return queryset
 
 
 def get_all_events_by_group() -> Group:
-    """Traga todos os grupos que contenham alguem que possua um agente que possuem eventos do tipo information"""
+    """Traga todos os grupos que contenham alguem que possua um agente
+    que possuem eventos do tipo information"""
     queryset = Group.objects.filter(user__agent__event__level="information")
     return queryset
